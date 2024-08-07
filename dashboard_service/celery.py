c392dcb1c9eef20be137f7052e726ce248c0401c
pikann <hvhai22@gmail.com>
2024-08-07 15:15:00 +0700

    feat: initialize dashboard service

---
+++ dashboard_service/celery.py
@@ -0,0 +1,24 @@
+import importlib.util
+import os
+import sys
+
+if importlib.util.find_spec("common") is None:
+    sys.path.append(
+        os.path.abspath(os.path.join("..", "pkg"))
+    )  # Import pkg without install
+
+from celery import Celery
+from common.celery.routing import (
+    setup_organization_task_routing,
+    setup_synchronous_model_task_routing,
+)
+from django.conf import settings
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dashboard_service.settings.local")
+app = Celery("dashboard_service")
+app.config_from_object("django.conf:settings", namespace="CELERY")
+
+setup_organization_task_routing()
+setup_synchronous_model_task_routing()
+
+app.autodiscover_tasks(settings.CELERY_TASKS)
---
+++ docker-entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/ash
+
+echo "Apply database migrations"
+python manage.py migrate_schemas --shared
+
+echo "Starting server"
+gunicorn --bind 0.0.0.0:80 --access-logfile - dashboard_service.wsgi & celery -A dashboard_service worker -l info -c 1
