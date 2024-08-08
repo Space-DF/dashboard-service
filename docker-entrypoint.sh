366b2070ad59f25cdc707e810e013cfb44ca5688
pikann <hvhai22@gmail.com>
2024-08-08 17:48:11 +0700

    chore: use gevent worker

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,4 +4,4 @@ echo "Apply database migrations"
 python manage.py migrate_schemas --shared
 
 echo "Starting server"
-gunicorn --bind 0.0.0.0:80 --access-logfile - dashboard_service.wsgi & celery -A dashboard_service worker -l info -c 1
+gunicorn --worker-class gevent --bind 0.0.0.0:80 --access-logfile - dashboard_service.wsgi & celery -A dashboard_service worker -l info -c 1
