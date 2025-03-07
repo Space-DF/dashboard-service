848b1644109aff1feab9577ad8af3bd76a274c21
pikann <hvhai22@gmail.com>
2025-03-07 11:51:31 +0700

    chore: remove authentication

--- device_state/views.py
+++ device_state/views.py
@@ -1,6 +1,4 @@
-from common.apps.space_role.constants import SpacePermission
 from common.pagination.base_pagination import BasePagination
-from common.permissions.permission_classes import has_space_permission_access
 from common.views.space import SpaceListAPIView
 from device_state.models import (
     DailyDeviceState,
@@ -16,15 +14,10 @@ from device_state.serializers import (
 )
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
-from rest_framework.permissions import IsAuthenticated
 
 
 class ListDeviceStateView(SpaceListAPIView):
     space_field = "space"
-    permission_classes = [
-        IsAuthenticated,
-        has_space_permission_access(SpacePermission.READ_DEVICE_STATE),
-    ]
     pagination_class = BasePagination
     filter_backends = [OrderingFilter, DjangoFilterBackend]
     ordering_fields = ["from_timestamp"]
