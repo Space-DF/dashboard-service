108f5696efd0438c29845f1421268b5fdc26f9fa
ngovinh2k2 <vinh.ngo@digitalfortress.dev>
2026-02-13 13:46:10 +0700

    feat(core): initialize codebase and implement core use cases
    Co-authored-by: Vinh Ngo <vinh.ngo@df.technology>
    Co-authored-by: Dat Le <dat.le@df.technology>
    Co-authored-by: Khoa Tran <khoa.tran@df.technology>
    Co-authored-by: Luu Le <luu.le@df.technology>
    Co-authored-by: Quynh Nguyen <quynh.nguyen@df.technology>

---
+++ apps/dashboard/constants.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class DisplayType(models.TextChoices):
+    GAUGE_TYPE = "gauge"
+    CHART_TYPE = "chart"
+    MAP_TYPE = "map"
+    SLIDER_TYPE = "slider"
+    SWITCH_TYPE = "switch"
+    VALUE_TYPE = "value"
+    HISTOGRAM_TYPE = "histogram"
+    TABLE_TYPE = "table"
+
+
+class WidgetPeriod(models.TextChoices):
+    HOUR = "hour"
+    DAY = "day"
+    WEEK = "week"
+    MONTH = "month"
---
+++ apps/dashboard/urls.py
@@ -0,0 +1,39 @@
+from django.urls import path
+
+from apps.dashboard.views import (
+    BulkCreateWidgetView,
+    BulkUpdateWidgetView,
+    ListCreateDashboardView,
+    ListCreateWidgetView,
+    UpdateDeleteDashboardView,
+    UpdateDeleteWidgetView,
+)
+
+app_name = "dashboard"
+
+urlpatterns = [
+    path(
+        "dashboards",
+        ListCreateDashboardView.as_view(),
+    ),
+    path(
+        "dashboards/<str:dashboard_id>/widgets/bulk-create",
+        BulkCreateWidgetView.as_view(),
+    ),
+    path(
+        "dashboards/<str:dashboard_id>/widgets/bulk-update",
+        BulkUpdateWidgetView.as_view(),
+    ),
+    path(
+        "dashboards/<str:dashboard_id>/widgets",
+        ListCreateWidgetView.as_view(),
+    ),
+    path(
+        "dashboards/<str:dashboard_id>/widgets/<str:id>",
+        UpdateDeleteWidgetView.as_view(),
+    ),
+    path(
+        "dashboards/<str:id>",
+        UpdateDeleteDashboardView.as_view(),
+    ),
+]
