729f8bcfcf565eea206b4dc8e5d5c717437f4064
ngovinh2k2 <vinh.ngo@digitalfortress.dev>
2026-05-04 11:37:53 +0700

    test: optimize performance for migration

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,10 +6,7 @@ echo "Apply database migrations"
 python manage.py migrate_schemas --shared
 
 # tenant schemas with parallel executor
-python manage.py migrate_schemas \
-  --executor=parallel \
-  --processes=5 \
-  --noinput
+python manage.py migrate_schemas --executor=parallel --noinput
 
 echo "Starting server"
 gunicorn --worker-class gevent --bind 0.0.0.0:80 --access-logfile - dashboard_service.wsgi & celery -A dashboard_service worker -l info -c 1
