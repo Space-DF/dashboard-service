16224ca8b168cefc7cf224875ca9af25da9b3642
lethanhdat762003 <dat.le@df.technology>
2026-07-16 15:40:46 +0700

    chore: read only fields

--- apps/dashboard/serializers.py
+++ apps/dashboard/serializers.py
@@ -14,7 +14,8 @@ telemetry_client = TelemetryServiceClient()
 class DashboardSerializer(serializers.ModelSerializer):
     class Meta:
         model = Dashboard
-        fields = ["id", "name", "created_at", "updated_at", "is_deactivated"]
+        fields = ["id", "name", "created_at", "updated_at"]
+        read_only_fields = ["is_deactivated"]
         extra_kwargs = {
             "id": {"read_only": True},
             "created_at": {"read_only": True},
